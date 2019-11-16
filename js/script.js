b6ef9bf9bd95956400f2568391c1a15450ac32d1
Patito <patriciabaez1987@gmail.com>
2019-11-16 19:36:37 -0300

    fetch en el registro

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 window.addEventListener("load", function(){
 
-  var anio= document.querySelector(".anio");
-
-  anio.addEventListener("click", function(){
-    fetch("../json/fechas.json")
+  provincias.addEventListener("click", function(){
+    fetch("https://apis.datos.gob.ar/georef/api/provincias")
     .then(function(respuesta){
         return respuesta.json();
     })
     .then(function(data){
-        console.log(data);
+        var provincias = document.querySelector("select#provincias");
+        for( var i=0; i < data.provincias.length; i++){
+        provincias.innerHTML += "<option value ='" +data.provincias[i].id +"'>" + data.provincias[i].nombre + "</option>";
+     }
 
     })
-
-  });
+    .catch(function(error){
+      console.log(error)
+    });
+ });
 
 
 });
